06f2f83d37efe5dfa15e1ca71be060ab75e7b7b1
songz1 <21pori30@gmail.com>
2019-04-19 01:32:34 +0900

    update HtmlUnitTest_04

--- src/main/java/com/example/study/HtmlUnitTest_04.java
+++ src/main/java/com/example/study/HtmlUnitTest_04.java
@@ -1,22 +1,27 @@
 package com.example.study;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import java.net.URL;
+
 public class HtmlUnitTest_04 {
     public static void main(String[] args) {
-        WebClient webClient = new WebClient();
+        WebClient webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
         try {
-            HtmlPage page = (HtmlPage) webClient.getPage("https://ma-andover.myfollett.com/aspen/logon.do");
-            HtmlForm form = page.getFormByName("logonForm");
-            form.getInputByName("username").setValueAttribute("myUsername");
-            HtmlInput passWordInput = form.getInputByName("password");
-            passWordInput.removeAttribute("disabled");
-            passWordInput.setValueAttribute("myPassword");
+            webClient.getOptions().setUseInsecureSSL(true);
+            HtmlPage page = webClient.getPage("https://forest.skhu.ac.kr/Gate/UniLogin.aspx");
 
-            page = form.getInputByValue("Log On").click(); // works fine
+            HtmlForm form = page.getFormByName("form1");
+            form.getInputByName("txtID").setValueAttribute("201532020");
+            HtmlInput passWordInput = form.getInputByName("9wldsl9^^");
+            passWordInput.removeAttribute("txtPW");
+            passWordInput.setValueAttribute("txtPW");
+
+            page = form.getInputByValue("ibtnLogin").click(); // works fine
 
             System.out.println(page.asText());
         } catch (Exception e) {
