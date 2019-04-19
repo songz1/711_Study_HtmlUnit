8923d16268f2dae9105be4f47e422f00d79aa74b
songz1 <21pori30@gmail.com>
2019-04-20 00:15:41 +0900

    update HtmlUnitTest_04

--- src/main/java/com/example/study/HtmlUnitTest_04.java
+++ src/main/java/com/example/study/HtmlUnitTest_04.java
@@ -2,11 +2,12 @@ package com.example.study;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
-import java.net.URL;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HtmlUnitTest_04 {
     public static void main(String[] args) {
@@ -15,15 +16,26 @@ public class HtmlUnitTest_04 {
             webClient.getOptions().setUseInsecureSSL(true);
             HtmlPage page = webClient.getPage("https://forest.skhu.ac.kr/Gate/UniLogin.aspx");
 
-            HtmlForm form = page.getFormByName("form1");
-            form.getInputByName("txtID").setValueAttribute("");
-            HtmlInput passWordInput = form.getInputByName("");
-            passWordInput.removeAttribute("txtPW");
-            passWordInput.setValueAttribute("txtPW");
+//            HtmlTextInput inputId = page.getFirstByXPath("//INPUT[@id='txtID']"); // ID 입력 필드
+//            HtmlPasswordInput inputPassword = page.getFirstByXPath("//INPUT[@id='txtPW']"); // 암호 입력 필드
+//            HtmlImageInput inputSubmit = page.getFirstByXPath("//INPUT[@id='txtID']"); // 로그인버튼
+//            HtmlSubmitInput submitBtn = page.getFirstByXPath("//INPUT[@id='txtID']"); // 로그인버튼
+//
+//            inputId.setText(""); // ID 입력
+//            inputPassword.setText(""); // 비밀번호 입력
+//            inputSubmit.click(); // 로그인 버튼 클릭
+//            page = submitBtn.click();
 
-            page = form.getInputByValue("ibtnLogin").click(); // works fine
+//            WebDriver driver = new HtmlUnitDriver(false);
+//            WebElement pw = driver.findElement(By.id(inputId));
+//            pw.sendKeys("9wldsl9^^");
+//            pw.sendKeys(Keys.ENTER);
 
-            System.out.println(page.asText());
+            System.out.println("1: "+page.asText());
+
+            HtmlPage page2 = webClient.getPage("https://forest.skhu.ac.kr/Gate/UniMyMain.aspx");
+
+            System.out.println("2: "+page2.asText());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
