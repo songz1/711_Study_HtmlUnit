ebae1e1cd461160018036155b23b8d7831b381fc
songz1 <21pori30@gmail.com>
2019-04-20 17:38:53 +0900

    add HtmlUnitTest_06

--- src/main/java/com/example/study/HtmlUnitTest_06.java
+++ src/main/java/com/example/study/HtmlUnitTest_06.java
@@ -5,7 +5,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class HtmlUnitTest_05 {
+public class HtmlUnitTest_06 {
     public static void main(String[] args) {
         WebClient webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
         try {
@@ -17,13 +17,9 @@ public class HtmlUnitTest_05 {
             form.getInputByName("txtPW").setValueAttribute("");
 
             HtmlPage homepage = form.getInputByName("ibtnLogin").click();
-            String Stringpage = homepage.getWebResponse().getContentAsString();
-
-            System.out.println("0: "+Stringpage);
             System.out.println("1: "+page.asText());
 
-            HtmlPage page3 = webClient.getPage("https://forest.skhu.ac.kr/Gate/UniMyMain.aspx");
-
+            HtmlPage page3 = webClient.getPage("https://forest.skhu.ac.kr/Gate/SAM/Lesson/G/SSEG20P.aspx?&maincd=O&systemcd=S&seq=100");
             System.out.println("3: "+page3.asText());
         } catch (Exception e) {
             e.printStackTrace();
