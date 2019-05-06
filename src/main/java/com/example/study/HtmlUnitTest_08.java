991b98d5dc69eaf4da958e1da1d926b688d5bc2e
songz1 <21pori30@gmail.com>
2019-05-06 23:35:53 +0900

    add HtmlUnitTest_08

--- src/main/java/com/example/study/HtmlUnitTest_08.java
+++ src/main/java/com/example/study/HtmlUnitTest_08.java
@@ -3,23 +3,21 @@ package com.example.study;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Set;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.util.Cookie;
 
-public class HtmlUnitTest_07 {
+public class HtmlUnitTest_08 {
 	public static void main(String[] args) {
 		IdPw idPw = new IdPw();
 		WebClient webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
-
 		try {
 			final URL forestBaseUrl = new URL("https://forest.skhu.ac.kr");
 			final URL loginPageUrl = new URL(forestBaseUrl + "/Gate/UniLogin.aspx");
 			final URL mainPageUrl = new URL(forestBaseUrl + "/Gate/UniMyMain.aspx");
+			final URL rentPageUrl = new URL(forestBaseUrl + "/Gate/SAM/Lesson/G/SSEG20P.aspx");
 
 			webClient.getOptions().setUseInsecureSSL(true);
 			HtmlPage loginPage = webClient.getPage(loginPageUrl);
@@ -27,22 +25,18 @@ public class HtmlUnitTest_07 {
 			HtmlForm loginForm = loginPage.getFormByName("");
 			loginForm.getInputByName("txtID").setValueAttribute(idPw.getId());
 			loginForm.getInputByName("txtPW").setValueAttribute(idPw.getPw());
-
 			HtmlPage mainPage = loginForm.getInputByName("ibtnLogin").click();
 
-			Set<Cookie> cookies = webClient.getCookieManager().getCookies();
-			System.out.println(cookies);
-			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-			if (!mainPageUrl.equals(mainPage.getBaseURL())) {
-				System.out.println("f");
-				System.out.println(mainPage.getBaseURL());
-			}else{
-				System.out.println("t");
+			if(mainPageUrl.equals(mainPage.getBaseURL())) {
+				System.out.println("** good!");
+				HtmlPage rentPage = webClient.getPage(rentPageUrl);
+				
 			}
+
 
 		} catch (MalformedURLException e) {
 
-		} catch (IOException i ){
+		} catch (IOException i) {
 
 		}
 	}
