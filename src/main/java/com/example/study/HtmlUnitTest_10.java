4236424419ca078e2c5192ce1e916212456ba327
songz1 <21pori30@gmail.com>
2019-05-16 17:12:27 +0900

    update HtmlUnitTest_10

--- src/main/java/com/example/study/HtmlUnitTest_10.java
+++ src/main/java/com/example/study/HtmlUnitTest_10.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebWindow;
 import com.gargoylesoftware.htmlunit.html.*;
+import net.sourceforge.htmlunit.corejs.javascript.Node;
 
 public class HtmlUnitTest_10 {
 	static class Basic {
@@ -62,28 +62,43 @@ public class HtmlUnitTest_10 {
 		HtmlPage mainPage = loginForm.getInputByName("ibtnLogin").click();
 		System.out.println("main: " + mainPage.asXml());
 
-		HtmlPage test = frame2(mainPage);
+		HtmlElement frameset1 = mainPage.getBody();
+		DomNode node = frameset1.getLastChild().getPreviousSibling();
+		HtmlFrame fr1 = (HtmlFrame) node;
+		FrameWindow fw1 = (FrameWindow) fr1.getEnclosedWindow();
+		HtmlPage p1 = (HtmlPage) fw1.getEnclosedPage();
+//		System.out.println("p1 "+p1.asXml());
 
-		System.out.println(test.asXml());
+		HtmlElement frameset2 = p1.getBody();
+		DomNode node2 = frameset2.getFirstChild().getNextSibling();
+		HtmlFrame fr2 = (HtmlFrame) node2;
+		FrameWindow fw2 = (FrameWindow) fr2.getEnclosedWindow();
+		HtmlPage p2 = (HtmlPage) fw2.getEnclosedPage();
+		System.out.println("p2 "+p2.asXml());
 
 
-//
-//		HtmlPage an = anchor.click();
-//		System.out.println("4: "+an.asXml());
+//div[@class='MenuDiv']/span[@id='menu']/table/tbody/tr[3]/td/span[@class='leftmenu2']/a
+
+
+//		HtmlPage test = frame2(basic, mainPage);
+//		System.out.println(test.asXml());
+
+
 	}
 
-	static HtmlPage frame2(HtmlPage mainPage) throws Exception {
-//		HtmlPage framePage2 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniMainOnestop.aspx");
-		HtmlPage framePage2 = mainPage.getFrameByName("mainFrame").getEnclosingPage();
+	static HtmlPage frame2(Basic basic, HtmlPage mainPage) throws Exception {
+		HtmlPage framePage2 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniMainOnestop.aspx");
+//		HtmlPage framePage2 = mainPage.getFrameByName("mainFrame").getEnclosingPage();
+
 		System.out.println("2: " + framePage2.asXml());
-		return frame3(framePage2);
+		return frame3(basic, framePage2).getFrameByName("mainFrame").getEnclosingPage();
 	}
 
-	static HtmlPage frame3(HtmlPage framePage2) throws Exception {
-//		HtmlPage framePage3 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniLeftMenuOneStop.aspx");
-		HtmlPage framePage3 = framePage2.getFrameByName("leftFrame").getEnclosingPage();
+	static HtmlPage frame3(Basic basic, HtmlPage framePage2) throws Exception {
+		HtmlPage framePage3 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniLeftMenuOneStop.aspx");
+//		HtmlPage framePage3 = framePage2.getFrameByName("leftFrame").getEnclosingPage();
 		System.out.println("3: " + framePage3.asXml());
-		return menu(framePage3);
+		return menu(framePage3).getFrameByName("leftFrame").getEnclosingPage();
 	}
 
 	static HtmlPage menu(HtmlPage framePage3) throws Exception {
@@ -95,7 +110,7 @@ public class HtmlUnitTest_10 {
 			if (anchor.asText().equals("웹서비스"))
 				link = anchor;
 		}
-		HtmlPage webService = link.click();
+		HtmlPage webService = (HtmlPage)link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
 		anchors = webService.getAnchors();
 		for(HtmlAnchor anchor : anchors){
 			String str = anchor.asText();
@@ -103,6 +118,6 @@ public class HtmlUnitTest_10 {
 			if (anchor.asText().equals("[N]시설물대여 신청"))
 				link = anchor;
 		}
-		return link.click();
+		return (HtmlPage)link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
 	}
 }
