5bf966ae8502b66cb2c068dc77878210c6b1e3d9
songz1 <21pori30@gmail.com>
2019-05-19 16:42:58 +0900

    update HtmlUnitTest_10

--- src/main/java/com/example/study/HtmlUnitTest_10.java
+++ src/main/java/com/example/study/HtmlUnitTest_10.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.List;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 import net.sourceforge.htmlunit.corejs.javascript.Node;
@@ -18,7 +19,7 @@ public class HtmlUnitTest_10 {
 
 		public Basic() throws Exception {
 			webClient.getOptions().setUseInsecureSSL(true);
-			webClient.getOptions().setJavaScriptEnabled(false);
+			webClient.getOptions().setJavaScriptEnabled(true);
 			webClient.getOptions().setCssEnabled(false);
 		}
 
@@ -67,17 +68,49 @@ public class HtmlUnitTest_10 {
 		HtmlFrame fr1 = (HtmlFrame) node;
 		FrameWindow fw1 = (FrameWindow) fr1.getEnclosedWindow();
 		HtmlPage p1 = (HtmlPage) fw1.getEnclosedPage();
-//		System.out.println("p1 "+p1.asXml());
+		System.out.println("p1 "+p1.asXml());
 
 		HtmlElement frameset2 = p1.getBody();
 		DomNode node2 = frameset2.getFirstChild().getNextSibling();
 		HtmlFrame fr2 = (HtmlFrame) node2;
 		FrameWindow fw2 = (FrameWindow) fr2.getEnclosedWindow();
 		HtmlPage p2 = (HtmlPage) fw2.getEnclosedPage();
-		System.out.println("p2 "+p2.asXml());
+		System.out.println("p2 " + p2.asXml());
+
+		HtmlAnchor menu0 = p2.getAnchorByText("웹서비스");
+		p2 = menu0.click();
+		System.out.println("p22 " + p2.asXml());
+
+//		ScriptResult scriptResult = p2.executeJavaScript("OnMenu(2);");
+//		System.out.println("scr2 " + scriptResult);
+
+		HtmlAnchor menu2 = p2.getAnchorByText("[N]시설물대여 신청");
+		menu0.click();
+//		System.out.println("p222 " + p2.asXml());
+
+		System.out.println("p1 "+p1.asXml());
 
 
-//div[@class='MenuDiv']/span[@id='menu']/table/tbody/tr[3]/td/span[@class='leftmenu2']/a
+//		ScriptResult scriptResult = p2.executeJavaScript("SetMenu(arrMenu0, 0, __maincd)");
+//		System.out.println("scr " + scriptResult);
+//		Object result = p2.executeJavaScript("OnMenu(2);ExecuteProgram(2);").getJavaScriptResult();
+//		p2.getPage();
+//		System.out.println("result: " + result + "\n" + p2.getPage());
+
+
+
+//		for (final HtmlTableBody b1 : t1.getBodies()) {
+//			for (final HtmlTableRow row : b1.getRows()) {
+//				System.out.println("Found row");
+//				for (final HtmlTableCell cell : row.getCells()) {
+//					System.out.println("   Found cell: " + cell.asText());
+//				}
+//			}
+//		}
+
+
+//		System.out.println("d1 "+d1.getWholeText());
+//		System.out.println("s1 "+s1.getId());
 
 
 //		HtmlPage test = frame2(basic, mainPage);
@@ -107,17 +140,19 @@ public class HtmlUnitTest_10 {
 		for (HtmlAnchor anchor : anchors) {
 			String str = anchor.asText();
 			System.out.println("** " + str);
-			if (anchor.asText().equals("웹서비스"))
+			if (anchor.asText().equals("웹서비스")) {
 				link = anchor;
+			}
 		}
-		HtmlPage webService = (HtmlPage)link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
+		HtmlPage webService = (HtmlPage) link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
 		anchors = webService.getAnchors();
-		for(HtmlAnchor anchor : anchors){
+		for (HtmlAnchor anchor : anchors) {
 			String str = anchor.asText();
-			System.out.println("** "+str);
-			if (anchor.asText().equals("[N]시설물대여 신청"))
+			System.out.println("** " + str);
+			if (anchor.asText().equals("[N]시설물대여 신청")) {
 				link = anchor;
+			}
 		}
-		return (HtmlPage)link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
+		return (HtmlPage) link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
 	}
 }
