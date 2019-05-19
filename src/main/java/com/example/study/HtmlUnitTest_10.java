cca943a117e77a5236784f9bb467c3baa540a732
songz1 <21pori30@gmail.com>
2019-05-19 17:22:09 +0900

    update HtmlUnitTest_10

--- src/main/java/com/example/study/HtmlUnitTest_10.java
+++ src/main/java/com/example/study/HtmlUnitTest_10.java
@@ -63,32 +63,23 @@ public class HtmlUnitTest_10 {
 		HtmlPage mainPage = loginForm.getInputByName("ibtnLogin").click();
 		System.out.println("main: " + mainPage.asXml());
 
-		HtmlElement frameset1 = mainPage.getBody();
-		DomNode node = frameset1.getLastChild().getPreviousSibling();
-		HtmlFrame fr1 = (HtmlFrame) node;
-		FrameWindow fw1 = (FrameWindow) fr1.getEnclosedWindow();
-		HtmlPage p1 = (HtmlPage) fw1.getEnclosedPage();
-		System.out.println("p1 "+p1.asXml());
+		HtmlElement frameset0 = mainPage.getBody();
+		DomNode node0 = frameset0.getLastChild().getPreviousSibling();
+		HtmlFrame fr0 = (HtmlFrame) node0;
+		FrameWindow fw0 = (FrameWindow) fr0.getEnclosedWindow();
+		HtmlPage p0 = (HtmlPage) fw0.getEnclosedPage();
+		System.out.println("p0 "+p0.asXml());
 
-		HtmlElement frameset2 = p1.getBody();
-		DomNode node2 = frameset2.getFirstChild().getNextSibling();
-		HtmlFrame fr2 = (HtmlFrame) node2;
-		FrameWindow fw2 = (FrameWindow) fr2.getEnclosedWindow();
-		HtmlPage p2 = (HtmlPage) fw2.getEnclosedPage();
-		System.out.println("p2 " + p2.asXml());
+		frame1(mainPage);
 
-		HtmlAnchor menu0 = p2.getAnchorByText("웹서비스");
-		p2 = menu0.click();
-		System.out.println("p22 " + p2.asXml());
+		HtmlElement frameset01 = p0.getBody();
+		DomNode node01 = frameset01.getLastChild().getPreviousSibling();
+		HtmlFrame fr01 = (HtmlFrame) node01;
+		FrameWindow fw01 = (FrameWindow) fr01.getEnclosedWindow();
+		HtmlPage p01 = (HtmlPage) fw01.getEnclosedPage();
+		System.out.println("p01 " + p01.asXml());
 
-//		ScriptResult scriptResult = p2.executeJavaScript("OnMenu(2);");
-//		System.out.println("scr2 " + scriptResult);
 
-		HtmlAnchor menu2 = p2.getAnchorByText("[N]시설물대여 신청");
-		menu0.click();
-//		System.out.println("p222 " + p2.asXml());
-
-		System.out.println("p1 "+p1.asXml());
 
 
 //		ScriptResult scriptResult = p2.executeJavaScript("SetMenu(arrMenu0, 0, __maincd)");
@@ -119,21 +110,45 @@ public class HtmlUnitTest_10 {
 
 	}
 
-	static HtmlPage frame2(Basic basic, HtmlPage mainPage) throws Exception {
-		HtmlPage framePage2 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniMainOnestop.aspx");
-//		HtmlPage framePage2 = mainPage.getFrameByName("mainFrame").getEnclosingPage();
+	static void frame1(HtmlPage mainPage) throws Exception {
+		HtmlElement frameset1 = mainPage.getBody();
+		DomNode node = frameset1.getLastChild().getPreviousSibling();
+		HtmlFrame fr1 = (HtmlFrame) node;
+		FrameWindow fw1 = (FrameWindow) fr1.getEnclosedWindow();
+		HtmlPage p1 = (HtmlPage) fw1.getEnclosedPage();
+		System.out.println("p1 "+p1.asXml());
+		frame2(p1);
 
-		System.out.println("2: " + framePage2.asXml());
-		return frame3(basic, framePage2).getFrameByName("mainFrame").getEnclosingPage();
+		return ;
 	}
 
-	static HtmlPage frame3(Basic basic, HtmlPage framePage2) throws Exception {
-		HtmlPage framePage3 = basic.getWebClient().getPage(basic.getForestBaseUrl() + "/Gate/UniLeftMenuOneStop.aspx");
-//		HtmlPage framePage3 = framePage2.getFrameByName("leftFrame").getEnclosingPage();
-		System.out.println("3: " + framePage3.asXml());
-		return menu(framePage3).getFrameByName("leftFrame").getEnclosingPage();
+	static void frame2(HtmlPage p1) throws Exception {
+		HtmlElement frameset2 = p1.getBody();
+		DomNode node2 = frameset2.getFirstChild().getNextSibling();
+		HtmlFrame fr2 = (HtmlFrame) node2;
+		FrameWindow fw2 = (FrameWindow) fr2.getEnclosedWindow();
+		HtmlPage p2 = (HtmlPage) fw2.getEnclosedPage();
+		System.out.println("p2 " + p2.asXml());
+		menu(p2);
+
+		return ;
 	}
 
+	static void menu(HtmlPage p2) throws Exception {
+		HtmlAnchor menu0 = p2.getAnchorByText("웹서비스");
+		p2 = menu0.click();
+		System.out.println("p22 " + p2.asXml());
+
+//		ScriptResult scriptResult = p2.executeJavaScript("OnMenu(2);");
+//		System.out.println("scr2 " + scriptResult);
+
+		HtmlAnchor menu2 = p2.getAnchorByText("[N]시설물대여 신청");
+		menu2.click();
+
+		return ;
+	}
+
+	/*
 	static HtmlPage menu(HtmlPage framePage3) throws Exception {
 		List<HtmlAnchor> anchors = framePage3.getAnchors();
 		HtmlAnchor link = null;
@@ -155,4 +170,5 @@ public class HtmlUnitTest_10 {
 		}
 		return (HtmlPage) link.click().getEnclosingWindow().getTopWindow().getEnclosedPage();
 	}
+	*/
 }
